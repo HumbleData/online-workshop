5221f293100e7fc267c9e4f056a9e87b7f303ef0
laisbsc <lais.carvalho@python.ie>
2023-07-08 14:35:12 +0100

    adapt solutions from notebook #4 to jupyterlite

--- content/solutions/04_02.py
+++ content/solutions/04_02.py
@@ -1,2 +1,4 @@
+dict_greeting = {'Namibia':'Hallo', 'France':'Bonjour', 'Spain':'Ola', 'UK':'Hello', 'Italy':'Ciao'}
+
 dict_greeting["UK"] = "Good Morning"
 print(dict_greeting)
--- content/solutions/04_06.py
+++ content/solutions/04_06.py
@@ -8,3 +8,5 @@
 def f(x):
     """Returns the argument multiplied by 3 and increased by 10."""
     return (x * 3) + 10
+
+print(f(4)) # x = 2 is arbitrary. Try with other values.
